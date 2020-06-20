9303277f96160e0815ec6e161703467b0e2dfc92
faithOnly <firdevssyildirimm@gmail.com>
2020-06-20 15:52:53 -0500

    all done except diamond shape

--- problems/11-splice-element/11-splice-element.js
+++ problems/11-splice-element/11-splice-element.js
@@ -3,5 +3,14 @@ Define function spliceElement(someArr, index)
 Splice the element at the provided index in the array.
 */
 function spliceElement(someArr, index) {
-  
+    //myArr = someArr[index]
+    let a = someArr.length-1
+    someArr = someArr.splice(index, a)
+    return someArr;
 }
+
+let arr = ['ayse', 'fatma', 'betul', 'elif', 'demet', 'derya'];
+console.log(arr)
+console.log(spliceElement(arr, 3));
+
+
